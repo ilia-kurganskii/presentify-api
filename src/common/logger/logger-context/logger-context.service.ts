c5de9b6392e0a964ef5db8ac450abd22c11a5067
Ilya Kurganskii <ilya.kurganskii@gmail.com>
2020-12-31 00:52:23 +0400

    feat(logger): use header traceId

--- src/common/logger/logger-context/logger-context.service.ts
+++ src/common/logger/logger-context/logger-context.service.ts
@@ -1,13 +1,15 @@
-import { Injectable, Scope } from '@nestjs/common';
+import { Injectable, Scope, Request, Inject } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
+import { REQUEST } from '@nestjs/core';
+
 @Injectable({
   scope: Scope.REQUEST,
 })
 export class LoggerContextService {
   private readonly traceId = '';
 
-  public constructor() {
-    this.traceId = uuid();
+  public constructor(@Inject(REQUEST) request: Request) {
+    this.traceId = request.headers['X-Request-ID'] ?? uuid();
   }
 
   public getTraceId(): string {
